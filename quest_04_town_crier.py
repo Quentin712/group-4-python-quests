df49cd4d87820bf6a164b45886470bd292409e9f
piradukund1 <p.iradukund1@alustudent.com>
2026-06-28 19:56:13 +0200

    Complete Level 1 Variables and Printing quests

---
+++ quest_04_town_crier.py
@@ -0,0 +1,7 @@
+# Combining several variables into one message.
+
+city_name = "Eldermoor"
+current_year = 2026
+my_name = "Philbert"
+
+print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {my_name}.")
