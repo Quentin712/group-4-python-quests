97e748e98ef0c419d9f8b18f7c0423c4fa3678eb
lkamikazi1 <l.kamikazi1@alustudent.com>
2026-06-29 15:11:14 +0200

    quest_15_the_nested_riddle.py

--- quests/quest_15_the_nested_riddle.py
+++ quests/quest_15_the_nested_riddle.py
@@ -1,15 +1,23 @@
+#Asks user to choose their direction
 direction = input("GO 'LEFT' or 'RIGHT'? ")
 direction = direction.upper()
 
+#Check the direction choosen
 if direction == "LEFT":
+    #This is brought only if user chose left
     action = input ("Want to 'SWIM' or 'WAIT'? ").upper() 
     if action == "SWIM":
+        #If swim is selected
         print ("Found treasure")
     else:
+        #If wait is selected
         print ("Nothing found sadly!")
 
+#If the choosen direction is right
 elif direction == "RIGHT":
+    #This is the immediate response 
      print ("You got caught by pirates.")
 else:
+    #If user input something unkown
      print ("Got lost.")
 
