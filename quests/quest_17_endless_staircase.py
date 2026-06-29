e04fce162b27cf42f775590fc46d2ef6fa85a4d4
yann <y.sheja@alustudent.com>
2026-06-29 15:17:41 +0200

    Quest18: The Loop of Riddles

--- quests/quest_17_endless_staircase.py
+++ quests/quest_17_endless_staircase.py
@@ -1,4 +1,4 @@
-#In this quest, we want the block of code of keep executing until the count reaches to 5 
+#In this quest, we want the block of code to keep executing until the count reaches to 5 
 #In this we are going to use while-loop as it will keep looping or executing as long as the condition is true
 #Starting from zero
 count = 0 
