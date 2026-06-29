e5c0965205895405482d322531f4d2fa6063dacb
lkamikazi1 <l.kamikazi1@alustudent.com>
2026-06-29 14:51:35 +0200

    quest_11_the_guardian_of_the_bridge.py

--- quests/quest_11_the_guardian_of_the_bridge.py
+++ quests/quest_11_the_guardian_of_the_bridge.py
@@ -1,5 +1,7 @@
+#This is to ask the user for there name
 age = input ("Enter age:")
+#This is to make sure the user inputs a number meaning if its anything other than a number it shows error
 age = int (age)
-
+#This check if the age is 18 or older and if the condition is true it prints the message below
 if age >= 18:
     print("You are old enough to vote.")
